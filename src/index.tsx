685d34a14821e96e797979c04368829546da867a
Jona <llamasjonat@gmail.com>
2024-01-03 06:49:15 -0300

    fix: update components

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,17 @@
-export * from "./Button";
+// Atomic components
+export * from './Button';
+export * from './Flex';
+export * from './Divider';
+export * from './Heading';
+export * from './Text';
+export * from './Input';
+export * from './Input/Feedback';
+export * from './Input/InputGroup';
+export * from './Input/InputGroupRight';
+export * from './Input/InputWithLabel';
+export * from './Avatar';
+export * from './Avatar/AvatarImage';
+export * from './Alert';
+
+// Layout
+export * from './Container';
