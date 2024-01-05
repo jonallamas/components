ed2ca834da44aa9bd228803d2487db9cd5fc9341
Jona <llamasjonat@gmail.com>
2024-01-05 14:44:39 -0300

    fix(provider): structure for theme from styled-components

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,20 +1,25 @@
-import React, { ReactNode } from 'react';
-import { ThemeProvider } from 'styled-components';
+import React, { ReactNode, createContext } from 'react';
+import { ThemeProvider as Theme, StyleSheetManager } from 'styled-components';
 
 import { theme } from './theme';
 import { GlobalStyle } from './style';
 
-interface ProviderProps {
+interface ThemeProviderProps {
   children: ReactNode;
+  sheet: any;
 }
 
-export function Provider(props: ProviderProps) {
-  const { children } = props;
+export const ThemeProviderContext = createContext({});
+
+export function ThemeProvider(props: ThemeProviderProps) {
+  const { children, sheet } = props;
 
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </>
+    <ThemeProviderContext.Provider value={{ theme: 'dark' }}>
+      <StyleSheetManager sheet={sheet}>
+        <GlobalStyle />
+        <Theme theme={theme}>{children}</Theme>
+      </StyleSheetManager>
+    </ThemeProviderContext.Provider>
   );
 }
