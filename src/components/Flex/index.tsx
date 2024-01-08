6b82fd89fb2101541b8df48f206a13846ea034f7
Jona <llamasjonat@gmail.com>
2024-01-08 06:49:51 -0300

    fix(component): removed onClick for Flex

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -5,25 +5,10 @@ import { FlexProps } from './types';
 import { FlexPrimitive } from './style';
 
 export function Flex(props: FlexProps): JSX.Element {
-  const {
-    children,
-    gap = 0,
-    direction = 'row',
-    flex = 'initial',
-    justify = 'start',
-    align = 'start',
-    onClick = () => null,
-  } = props;
+  const { children, gap = 0, direction = 'row', flex = 'initial', justify = 'start', align = 'start' } = props;
 
   return (
-    <FlexPrimitive
-      onClick={onClick}
-      $gap={`${gap}px`}
-      $direction={direction}
-      $flex={flex}
-      $justify={justify}
-      $align={align}
-    >
+    <FlexPrimitive $gap={`${gap}px`} $direction={direction} $flex={flex} $justify={justify} $align={align} {...props}>
       {children}
     </FlexPrimitive>
   );
--- src/components/Flex/types.ts
+++ src/components/Flex/types.ts
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react';
 
 export interface FlexProps {
-  children: ReactNode;
+  children?: ReactNode;
   gap?: 0 | 2 | 4 | 8 | 16;
   direction?: 'row' | 'column';
   flex?: 0 | 1 | 'initial';
   justify?: 'start' | 'end' | 'space-between' | 'center';
   align?: 'start' | 'center' | 'end';
-  onClick?: (e: any) => void;
+  // onClick?: (e: any) => void;
 }
 
 export interface FlexPrimitiveProps {
