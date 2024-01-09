8b68939a0d4fa31cf27e80edc97ed9e1c3b558c2
Jona <llamasjonat@gmail.com>
2024-01-09 12:38:58 -0300

    assets: removed fonts from project

--- src/style.tsx
+++ src/style.tsx
@@ -2,34 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 import { theme } from './theme';
 
-import IAAB3 from './fonts/IAAB3.woff2';
-import SFRegular from './fonts/SF-Regular.woff2';
-import SFBold from './fonts/SF-Bold.woff2';
-
 export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'IAAB3-Mono';
-    src: url(${IAAB3}) format('truetype');
-    font-weight: 400;
-    font-style: normal;
-    font-display: auto;
-  }
-
-  @font-face {
-    font-family: 'SF PRO Text';
-    src: url(${SFRegular}) format('truetype');
-    font-weight: 400;
-    font-style: normal;
-    font-display: auto;
-  }
-
-  @font-face {
-    font-family: 'SF PRO Text';
-    src: url(${SFBold}) format('truetype');
-    font-weight: 700;
-    font-style: bold;
-    font-display: auto;
-  }
 
   * {
     box-sizing: border-box;
